3fc6a56e6d40d8229afe301b4eee460d9537f0aa
Fletcher Nichol <fnichol@nichol.ca>
2011-03-10 00:32:09 -0700

    Bump version to 0.5 to reflect attribute name changes.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.4"
+version          "0.5"
 %w(runit java iptables).each { |cb| depends cb }
