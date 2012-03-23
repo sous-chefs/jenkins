3596a50bf4307d5036abc6a999396222cb2d96dc
Eric Heydrick <eheydrick@gmail.com>
2012-03-23 16:03:18 -0700

    Update README reference in metadata to use md version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "AJ Christensen"
 maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6"
 
 %w(runit java).each { |cb| depends cb }
