f91495d92e520623bb2970a3c079294f23d575b1
AJ Christensen <aj@junglist.gen.nz>
2012-04-04 11:39:57 +1200

    0.6.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.2"
+version          "0.6.3"
 
 %w(runit java).each { |cb| depends cb }
 %w(iptables yum apt).each { |cb| recommends cb }
