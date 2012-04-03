b93c82cbf98d22c01fd9e56d2521347e373b596c
AJ Christensen <aj@junglist.gen.nz>
2012-04-04 10:25:07 +1200

    0.6.1: merge a few patches, create_if_missing for plugins

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "AJ Christensen"
+ maintainer       "AJ Christensen"
 maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6"
+version          "0.6.1"
 
 %w(runit java).each { |cb| depends cb }
 %w(iptables yum apt).each { |cb| recommends cb }
