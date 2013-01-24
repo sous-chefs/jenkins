e41bcf190a2c59a19cabc4af33914350be2869ed
Seth Chisamore <schisamo@opscode.com>
2013-01-24 15:35:23 -0500

    update metadata file with modern idioms
    * Ensure we have a 'name' attribute for things like Berkshelf
    * Move some dependencies to `recommends` and `suggests` attributes.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,16 @@
- maintainer       "AJ Christensen"
+name             "jenkins"
+maintainer       "AJ Christensen"
 maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.3"
 
-%w(runit java).each { |cb| depends cb }
-%w(iptables yum apt).each { |cb| recommends cb }
+depends "java"
+depends "runit"
+
+recommends "apt"
+
+suggests "apache2"
+suggests "nginx"
+suggests "iptables"
