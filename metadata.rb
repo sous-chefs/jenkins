6bc4785e60c2e0873eb19699fe383c336ed05d12
Seth Chisamore <schisamo@opscode.com>
2013-02-28 18:23:45 -0500

    bump to 0.9.9 dev version in preparation of impending 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.7.0"
+version          "0.9.9"
 
 depends "java"
 depends "runit", ">= 1.0.0"
