0fd63650e7feda7db5187088c8ce980643d96470
Seth Chisamore <schisamo@opscode.com>
2013-03-04 10:03:42 -0500

    setting version to 0.8.0
    The previous (non-released) bump to `0.9.9` was premature. Some inflight
    LWRP experiments may make the `1.0.0` a radical departure from the 
    existing versions.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.9.9"
+version          "0.8.0"
 
 depends "java"
 depends "runit", ">= 1.0.0"
