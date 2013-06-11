97144f1da6cf8618a087ba922427c8823a5e89f0
Adrian Moisey <adrian@yola.com>
2013-06-11 12:27:05 +0200

    Look for an existing .jpi before downloading a new plugin

--- recipes/server.rb
+++ recipes/server.rb
@@ -84,6 +84,7 @@ node['jenkins']['server']['plugins'].each do |plugin|
     action :create_if_missing
     notifies :restart, "runit_service[jenkins]"
     notifies :create, "ruby_block[block_until_operational]"
+    not_if do ::File.exists?("#{plugins_dir}/#{name}.jpi") end
   end
 end
 
