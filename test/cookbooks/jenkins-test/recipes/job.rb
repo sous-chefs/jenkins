e260ff6d3a73d405cf78c8eb18d4800039741ee7
Kannan Manickam <me@arangamani.net>
2013-08-14 02:42:30 -0700

    Added test for job provider

---
+++ test/cookbooks/jenkins-test/recipes/job.rb
@@ -0,0 +1,30 @@
+cookbook_file "/var/jenkins_test_job.xml" do
+  source "test_job.xml"
+end
+
+jenkins_job "test" do
+  config "/var/jenkins_test_job.xml"
+  action :create
+end
+
+jenkins_job "test" do
+  action :build
+end
+
+ruby_block "sleep 10" do
+  block do
+    sleep 10
+  end
+end
+
+jenkins_job "test" do
+  action :disable
+end
+
+jenkins_job "test" do
+  action :enable
+end
+
+jenkins_job "test" do
+  action :delete
+end
