sous-chefs/jenkins

commit 594e988850ca191ba6d1fe5c38fc75450bdcbefa

[COOK-2527] name all private recipes with a leading underscore
The emerging Chef community idiom is to name all "private" recipes with
a leading underscore. End users should only include one of the public
recipes in their run lists: server, node, proxy. In the context of this
cookbook the default recipe is also treated as no-op now.

We will also add the following new attributes

* `node['jenkins']['node']['agent_type']` - type of agent used to
  connect to slave nodes.
* `node['jenkins']['http_proxy`]['variant']` - variant/flavor of HTTP
  proxy to use.

recipes/_proxy_apache2.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: jenkins
-# Recipe:: proxy_nginx
+# Recipe:: _proxy_apache2
 #
 # Author:: Fletcher Nichol <fnichol@nichol.ca>
 #
@@ -12,47 +12,66 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in wrhiting, software
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
 
-include_recipe "nginx"
+include_recipe "apache2"
 
 www_redirect = (node['jenkins']['http_proxy']['www_redirect'] == "enable")
 host_name = node['jenkins']['http_proxy']['host_name'] || node['fqdn']
 
-template "#{node['nginx']['dir']}/htpasswd" do
+if node['jenkins']['http_proxy']['cas_validate_server'] == "cas"
+  apache_module "mod_auth_cas"
+end
+
+if node['jenkins']['http_proxy']['ssl']['enabled']
+  include_recipe "apache2::mod_ssl"
+end
+
+apache_module "proxy"
+apache_module "proxy_http"
+apache_module "vhost_alias"
+
+if www_redirect || node['jenkins']['http_proxy']['ssl']['redirect_http']
+  apache_module "rewrite"
+end
+
+template "#{node['apache']['dir']}/htpasswd" do
   variables( :username => node['jenkins']['http_proxy']['basic_auth_username'],
              :password => node['jenkins']['http_proxy']['basic_auth_password'])
-  owner node['nginx']['user']
-  group node['nginx']['user']
+  owner node['apache']['user']
+  group node['apache']['user']
   mode '0600'
 end
 
-template "#{node['nginx']['dir']}/sites-available/jenkins.conf" do
-  source      "nginx_jenkins.conf.erb"
+template "#{node['apache']['dir']}/sites-available/jenkins" do
+  source      "apache_jenkins.erb"
   owner       'root'
   group       'root'
   mode        '0644'
   variables(
     :host_name        => host_name,
     :host_aliases     => node['jenkins']['http_proxy']['host_aliases'],
-    :listen_ports     => node['jenkins']['http_proxy']['listen_ports'],
     :www_redirect     => www_redirect,
-    :max_upload_size  => node['jenkins']['http_proxy']['client_max_body_size'],
+    :jenkins_port     => node['jenkins']['server']['port'],
     :redirect_http    => node['jenkins']['http_proxy']['ssl']['redirect_http'],
     :ssl_enabled      => node['jenkins']['http_proxy']['ssl']['enabled'],
     :ssl_listen_ports => node['jenkins']['http_proxy']['ssl']['ssl_listen_ports']
   )
 
-  if File.exists?("#{node['nginx']['dir']}/sites-enabled/jenkins.conf")
-    notifies  :restart, 'service[nginx]'
+  if File.exists?("#{node['apache']['dir']}/sites-enabled/jenkins")
+    notifies  :restart, 'service[apache2]'
   end
 end
 
-nginx_site "jenkins.conf" do
+apache_site "000-default" do
+  enable  false
+end
+
+apache_site "jenkins" do
   enable true
 end

attributes/node.rb
@@ -26,12 +26,15 @@ case node['platform_family']
 when "windows"
   default['jenkins']['node']['home'] = "C:/jenkins"
   default['jenkins']['node']['log_dir']  = "C:/jenkins"
+  default['jenkins']['node']['agent_type'] = "windows"
 when "mac_os_x"
   default['jenkins']['node']['home'] = "/Users/jenkins"
   default['jenkins']['node']['log_dir']  = "/var/log/jenkins"
+  default['jenkins']['node']['agent_type'] = "jnlp"
 else
   default['jenkins']['node']['home'] = "/home/jenkins"
   default['jenkins']['node']['log_dir']  = "/var/log/jenkins"
+  default['jenkins']['node']['agent_type'] = "jnlp"
 end
 
 default['jenkins']['node']['user'] = "jenkins-node"

attributes/server.rb
@@ -55,6 +55,7 @@ default['jenkins']['http_proxy']['www_redirect']         = "disable"
 default['jenkins']['http_proxy']['listen_ports']         = [ 80 ]
 default['jenkins']['http_proxy']['host_name']            = nil
 default['jenkins']['http_proxy']['host_aliases']         = []
+default['jenkins']['http_proxy']['variant'] = "nginx"
 default['jenkins']['http_proxy']['client_max_body_size'] = "1024m"
 default['jenkins']['http_proxy']['basic_auth_username'] = "jenkins"
 default['jenkins']['http_proxy']['basic_auth_password'] = "jenkins"

recipes/_node_jnlp.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: jenkins
-# Recipe:: node_ssh
+# Recipe:: _node_jnlp
 #
 # Author:: Doug MacEachern <dougm@vmware.com>
 # Author:: Fletcher Nichol <fnichol@nichol.ca>
@@ -21,45 +21,22 @@
 #
 
 include_recipe "java"
+include_recipe "runit"
 
-unless Chef::Config[:solo]
-  unless node['jenkins']['server']['pubkey']
-    host = node['jenkins']['server']['host']
-    if host == node['fqdn']
-      host = URI.parse(node['jenkins']['server']['url']).host
-    end
-    jenkins_node = search('node', "fqdn:#{host}").first
-    node.set['jenkins']['server']['pubkey'] = jenkins_node['jenkins']['server']['pubkey']
-  end
-end
+service_name = "jenkins-slave"
+slave_jar = "#{node['jenkins']['node']['home']}/slave.jar"
 
-group node['jenkins']['node']['group']
+group node['jenkins']['node']['user']
 
 user node['jenkins']['node']['user'] do
-  comment "Jenkins CI node (ssh)"
+  comment "Jenkins CI node (jnlp)"
   gid node['jenkins']['node']['user']
   home node['jenkins']['node']['home']
-  shell node['jenkins']['node']['shell']
 end
 
 directory node['jenkins']['node']['home'] do
   owner node['jenkins']['node']['user']
   group node['jenkins']['node']['user']
-  action :create
-end
-
-directory "#{node['jenkins']['node']['home']}/.ssh" do
-  owner node['jenkins']['node']['user']
-  group node['jenkins']['node']['user']
-  mode '0700'
-  action :create
-end
-
-file "#{node['jenkins']['node']['home']}/.ssh/authorized_keys" do
-  content node['jenkins']['server']['pubkey']
-  owner node['jenkins']['node']['user']
-  group node['jenkins']['node']['user']
-  mode '0600'
   action :create
 end
 
@@ -69,14 +46,20 @@ jenkins_node node['jenkins']['node']['name'] do
   remote_fs    node['jenkins']['node']['home']
   labels       node['jenkins']['node']['labels']
   mode         node['jenkins']['node']['mode']
-  launcher     "ssh"
+  launcher     "jnlp"
+  mode         node['jenkins']['node']['mode']
   availability node['jenkins']['node']['availability']
-  env          node['jenkins']['node']['env']
-  #ssh options
-  host         node['jenkins']['node']['ssh_host']
-  port         node['jenkins']['node']['ssh_port']
-  username     node['jenkins']['node']['ssh_user']
-  password     node['jenkins']['node']['ssh_pass']
-  private_key  node['jenkins']['node']['ssh_private_key']
-  jvm_options  node['jenkins']['node']['jvm_options']
+end
+
+remote_file slave_jar do
+  source "#{node['jenkins']['server']['url']}/jnlpJars/slave.jar"
+  owner node['jenkins']['node']['user']
+  #only restart if slave.jar is updated
+  if ::File.exists?(slave_jar)
+    notifies :restart, "service[#{service_name}]", :immediately
+  end
+end
+
+runit_service service_name do
+  action :enable
 end

recipes/_node_ssh.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: jenkins
-# Recipe:: node_jnlp
+# Recipe:: _node_ssh
 #
 # Author:: Doug MacEachern <dougm@vmware.com>
 # Author:: Fletcher Nichol <fnichol@nichol.ca>
@@ -21,22 +21,45 @@
 #
 
 include_recipe "java"
-include_recipe "runit"
 
-service_name = "jenkins-slave"
-slave_jar = "#{node['jenkins']['node']['home']}/slave.jar"
+unless Chef::Config[:solo]
+  unless node['jenkins']['server']['pubkey']
+    host = node['jenkins']['server']['host']
+    if host == node['fqdn']
+      host = URI.parse(node['jenkins']['server']['url']).host
+    end
+    jenkins_node = search('node', "fqdn:#{host}").first
+    node.set['jenkins']['server']['pubkey'] = jenkins_node['jenkins']['server']['pubkey']
+  end
+end
 
-group node['jenkins']['node']['user']
+group node['jenkins']['node']['group']
 
 user node['jenkins']['node']['user'] do
-  comment "Jenkins CI node (jnlp)"
+  comment "Jenkins CI node (ssh)"
   gid node['jenkins']['node']['user']
   home node['jenkins']['node']['home']
+  shell node['jenkins']['node']['shell']
 end
 
 directory node['jenkins']['node']['home'] do
   owner node['jenkins']['node']['user']
   group node['jenkins']['node']['user']
+  action :create
+end
+
+directory "#{node['jenkins']['node']['home']}/.ssh" do
+  owner node['jenkins']['node']['user']
+  group node['jenkins']['node']['user']
+  mode '0700'
+  action :create
+end
+
+file "#{node['jenkins']['node']['home']}/.ssh/authorized_keys" do
+  content node['jenkins']['server']['pubkey']
+  owner node['jenkins']['node']['user']
+  group node['jenkins']['node']['user']
+  mode '0600'
   action :create
 end
 
@@ -46,20 +69,14 @@ jenkins_node node['jenkins']['node']['name'] do
   remote_fs    node['jenkins']['node']['home']
   labels       node['jenkins']['node']['labels']
   mode         node['jenkins']['node']['mode']
-  launcher     "jnlp"
-  mode         node['jenkins']['node']['mode']
+  launcher     "ssh"
   availability node['jenkins']['node']['availability']
-end
-
-remote_file slave_jar do
-  source "#{node['jenkins']['server']['url']}/jnlpJars/slave.jar"
-  owner node['jenkins']['node']['user']
-  #only restart if slave.jar is updated
-  if ::File.exists?(slave_jar)
-    notifies :restart, "service[#{service_name}]", :immediately
-  end
-end
-
-runit_service service_name do
-  action :enable
+  env          node['jenkins']['node']['env']
+  #ssh options
+  host         node['jenkins']['node']['ssh_host']
+  port         node['jenkins']['node']['ssh_port']
+  username     node['jenkins']['node']['ssh_user']
+  password     node['jenkins']['node']['ssh_pass']
+  private_key  node['jenkins']['node']['ssh_private_key']
+  jvm_options  node['jenkins']['node']['jvm_options']
 end

recipes/_node_windows.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: jenkins
-# Recipe:: node_windows
+# Recipe:: _node_windows
 #
 # Author:: Doug MacEachern <dougm@vmware.com>
 # Author:: Fletcher Nichol <fnichol@nichol.ca>

recipes/_proxy_nginx.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: jenkins
-# Recipe:: proxy_apache2
+# Recipe:: _proxy_nginx
 #
 # Author:: Fletcher Nichol <fnichol@nichol.ca>
 #
@@ -12,66 +12,47 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
+# Unless required by applicable law or agreed to in wrhiting, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
 
-include_recipe "apache2"
+include_recipe "nginx"
 
 www_redirect = (node['jenkins']['http_proxy']['www_redirect'] == "enable")
 host_name = node['jenkins']['http_proxy']['host_name'] || node['fqdn']
 
-if node['jenkins']['http_proxy']['cas_validate_server'] == "cas"
-  apache_module "mod_auth_cas"
-end
-
-if node['jenkins']['http_proxy']['ssl']['enabled']
-  include_recipe "apache2::mod_ssl"
-end
-
-apache_module "proxy"
-apache_module "proxy_http"
-apache_module "vhost_alias"
-
-if www_redirect || node['jenkins']['http_proxy']['ssl']['redirect_http']
-  apache_module "rewrite"
-end
-
-template "#{node['apache']['dir']}/htpasswd" do
+template "#{node['nginx']['dir']}/htpasswd" do
   variables( :username => node['jenkins']['http_proxy']['basic_auth_username'],
              :password => node['jenkins']['http_proxy']['basic_auth_password'])
-  owner node['apache']['user']
-  group node['apache']['user']
+  owner node['nginx']['user']
+  group node['nginx']['user']
   mode '0600'
 end
 
-template "#{node['apache']['dir']}/sites-available/jenkins" do
-  source      "apache_jenkins.erb"
+template "#{node['nginx']['dir']}/sites-available/jenkins.conf" do
+  source      "nginx_jenkins.conf.erb"
   owner       'root'
   group       'root'
   mode        '0644'
   variables(
     :host_name        => host_name,
     :host_aliases     => node['jenkins']['http_proxy']['host_aliases'],
+    :listen_ports     => node['jenkins']['http_proxy']['listen_ports'],
     :www_redirect     => www_redirect,
-    :jenkins_port     => node['jenkins']['server']['port'],
+    :max_upload_size  => node['jenkins']['http_proxy']['client_max_body_size'],
     :redirect_http    => node['jenkins']['http_proxy']['ssl']['redirect_http'],
     :ssl_enabled      => node['jenkins']['http_proxy']['ssl']['enabled'],
     :ssl_listen_ports => node['jenkins']['http_proxy']['ssl']['ssl_listen_ports']
   )
 
-  if File.exists?("#{node['apache']['dir']}/sites-enabled/jenkins")
-    notifies  :restart, 'service[apache2]'
+  if File.exists?("#{node['nginx']['dir']}/sites-enabled/jenkins.conf")
+    notifies  :restart, 'service[nginx]'
   end
 end
 
-apache_site "000-default" do
-  enable  false
-end
-
-apache_site "jenkins" do
+nginx_site "jenkins.conf" do
   enable true
 end

recipes/_server_package.rb
@@ -1,7 +1,7 @@
 #
 # Author:: Guilhem Lettron <guilhem.lettron@youscribe.com>
 # Cookbook Name:: jenkins
-# Recipe:: server_package
+# Recipe:: _server_package
 #
 # Copyright 2013, Youscribe
 #

commit e29fd5959a0ffb3cf1b39f223f8498edecb4ed94

Fix regression caused by 594e988850ca191ba6d1fe5c38fc75450bdcbefa
The `jenkins_port` variable was unset in the template, breaking the
proxy with Apache2.

Refs [COOK-3700](https://tickets.opscode.com/browse/COOK-3700)

recipes/_proxy_apache2.rb
@@ -57,6 +57,7 @@ template "#{node['apache']['dir']}/sites-available/jenkins" do
     :host_name        => host_name,
     :host_aliases     => node['jenkins']['http_proxy']['host_aliases'],
     :www_redirect     => www_redirect,
+    :jenkins_port     => node['jenkins']['server']['port'],
     :redirect_http    => node['jenkins']['http_proxy']['ssl']['redirect_http'],
     :ssl_enabled      => node['jenkins']['http_proxy']['ssl']['enabled'],
     :ssl_listen_ports => node['jenkins']['http_proxy']['ssl']['ssl_listen_ports']