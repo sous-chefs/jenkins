56a2781c856587234b9b34785f28c50a729a6d60
Jean-Francois Theroux <me@failshell.io>
2013-11-22 14:16:13 -0500

    renaming attribute
    - Rename the attribute to be consistent with the config option in the
      template.

--- attributes/server.rb
+++ attributes/server.rb
@@ -67,7 +67,7 @@ default['jenkins']['server']['url']  = "http://#{default['jenkins']['server']['h
 
 default['jenkins']['server']['plugins'] = []
 default['jenkins']['server']['jvm_options'] = nil
-default['jenkins']['server']['jenkins_options'] = nil
+default['jenkins']['server']['jenkins_args'] = nil
 default['jenkins']['server']['pubkey'] = nil
 
 default['jenkins']['http_proxy']['variant'] = 'nginx'
