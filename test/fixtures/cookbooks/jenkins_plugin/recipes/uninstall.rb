c2c5e9a6c26b7d4026480ed9ef90eda036d7d0de
Seth Vargo <sethvargo@gmail.com>
2014-01-07 21:05:19 -0500

    Simplify plugin tests to just restart Jenkins instead of blocking

--- test/fixtures/cookbooks/jenkins_plugin/recipes/uninstall.rb
+++ test/fixtures/cookbooks/jenkins_plugin/recipes/uninstall.rb
@@ -4,19 +4,7 @@ include_recipe 'jenkins::server'
 include_recipe 'jenkins_plugin::install'
 
 # Grr...
-service 'jenkins' do
-  action :restart
-end
-
-# Jenkins doesn't block after restarting, so we need to wait until the plugins
-# are fully installed from the last recipe, with a sensible timeout. 15 seconds
-# is entirely arbitrary, but was the lowest number I could go without the test
-# failing because Jenkins wasn't ready.
-#
-# Unrelated comment: Seriously Jenkins, what the actual fuck?
-ruby_block 'block_until_plugin_installed' do
-  block { sleep(15) }
-end
+jenkins_command 'restart'
 
 # Test basic job deletion
 jenkins_plugin 'greenballs' do
