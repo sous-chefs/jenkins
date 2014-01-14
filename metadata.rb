4081090463dfac335b4f09b70ecc20a8c7aecd70
Seth Vargo <sethvargo@gmail.com>
2014-01-14 16:29:17 -0500

    Version bump to 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'releng@getchef.com'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.3'
+version          '2.0.0'
 
 recipe 'master', 'Installs a Jenkins master'
 
