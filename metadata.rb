eaca70dd016e9662f29859c39c0e357554d44d6c
nBerg <berg@adobe.com>
2014-02-04 18:28:09 -0500

    Removed dependencies we don't need

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'releng@getchef.com'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 recipe 'master', 'Installs a Jenkins master'
 
-depends 'apt',   '~> 2.0'
-depends 'runit', '~> 1.5'
-depends 'yum',   '~> 3.0'
+#depends 'apt',   '~> 2.0'
+#depends 'runit', '~> 1.5'
+#depends 'yum',   '~> 3.0'
