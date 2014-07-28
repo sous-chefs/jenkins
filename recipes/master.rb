09f810c320607a10f48b5a48720605a124d6a419
Olivier Dolbeau <odolbeau@gmail.com>
2014-07-28 18:23:15 +0200

    Update master.rb
    Correct typo

--- recipes/master.rb
+++ recipes/master.rb
@@ -33,5 +33,5 @@ rescue Chef::Exceptions::RecipeNotFound
   raise Chef::Exceptions::RecipeNotFound, "The install method " \
     "`#{node['jenkins']['master']['install_method']}' is not supported by " \
     "this cookbook. Please ensure you have spelled it correctly. If you " \
-    "continue to encounter this error, please file an issue."
+    "continue to encounter this error, please fill an issue."
 end
