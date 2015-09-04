3fb54ee60d638e5e33b35e064711c7fc466096f7
John Mickey <jmickey@pithoslabs.com>
2015-09-04 10:27:39 -0500

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'releng@chef.io'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.4.0'
+version          '2.4.1'
 
 recipe 'master', 'Installs a Jenkins master'
 
