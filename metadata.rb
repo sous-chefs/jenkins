44beeba01198418758294bcd263e354e51c195ec
John Mickey <jmickey@pithoslabs.com>
2015-10-30 10:33:27 -0500

    Remove unwanted changes to chef/jenkins

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
 name             'jenkins'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'releng@chef.io'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.4.6'
+version          '2.4.1'
 
 source_url 'https://github.com/chef-cookbooks/jenkins' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/jenkins/issues' if respond_to?(:issues_url)
