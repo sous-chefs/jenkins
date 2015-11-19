3e6b55c8e70e4a56ee6181ce4e6202b872c927bc
John Mickey <jmickey@pithoslabs.com>
2015-11-19 08:44:49 -0600

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'releng@chef.io'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.4.1'
+version          '2.5.0'
 
 source_url 'https://github.com/chef-cookbooks/jenkins' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/jenkins/issues' if respond_to?(:issues_url)
