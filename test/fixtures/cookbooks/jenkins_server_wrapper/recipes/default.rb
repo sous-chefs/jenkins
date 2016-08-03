6bf72fc5963f3a080e9206012db7c0435df08745
Dieter Blomme <dieterblomme@gmail.com>
2016-08-03 11:21:52 +0200

    Fix for missing plugins in jenkins 2.x
    This reverts commit 4187004d167199539529eb29ac8747af01d2ba6d.

--- test/fixtures/cookbooks/jenkins_server_wrapper/recipes/default.rb
+++ test/fixtures/cookbooks/jenkins_server_wrapper/recipes/default.rb
@@ -13,3 +13,20 @@ else
   include_recipe 'jenkins::java'
   include_recipe 'jenkins::master'
 end
+
+# Install some plugins needed, but not installed on jenkins2 by default
+jenkins_plugins = %w(
+  mailer
+  credentials
+  ssh-credentials
+  ssh-slaves
+)
+jenkins_plugins.each do |plugin|
+  jenkins_plugin plugin do
+    notifies :execute, 'jenkins_command[safe-restart]', :immediately
+  end
+end
+
+jenkins_command 'safe-restart' do
+  action :nothing
+end
