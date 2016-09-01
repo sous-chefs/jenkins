5ce96ad11ecd3240044db58166dd3bf9ef36e49c
Tim Smith <tsmith@chef.io>
2016-08-31 20:06:00 -0700

    Add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,8 @@ version          '2.6.0'
 source_url 'https://github.com/chef-cookbooks/jenkins'
 issues_url 'https://github.com/chef-cookbooks/jenkins/issues'
 
+chef_version '>= 12' if respond_to?(:chef_version)
+
 recipe 'jenkins::master', 'Installs a Jenkins master'
 
 depends 'apt',   '>= 2.0'
