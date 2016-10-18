c49d5690cfc7bad607970585378778ac01aa7b45
Lamont Granquist <lamont@scriptkiddie.org>
2016-10-18 10:20:00 -0700

    replace manual creation of resource with build_resource
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- libraries/slave_jnlp.rb
+++ libraries/slave_jnlp.rb
@@ -146,10 +146,9 @@ class Chef
     # @return [Chef::Resource::Group]
     #
     def group_resource
-      return @group_resource if @group_resource
-      @group_resource = Chef::Resource::Group.new(new_resource.group, run_context)
-      @group_resource.system(node['jenkins']['master']['use_system_accounts'])
-      @group_resource
+      @group_resource ||= build_resource(:group, new_resource.group) do
+        system(node['jenkins']['master']['use_system_accounts'])
+      end
     end
 
     #
@@ -160,13 +159,12 @@ class Chef
     # @return [Chef::Resource::User]
     #
     def user_resource
-      return @user_resource if @user_resource
-      @user_resource = Chef::Resource::User.new(new_resource.user, run_context)
-      @user_resource.gid(new_resource.group)
-      @user_resource.comment('Jenkins slave user - Created by Chef')
-      @user_resource.home(new_resource.remote_fs)
-      @user_resource.system(node['jenkins']['master']['use_system_accounts'])
-      @user_resource
+      @user_resource ||= build_resource(:user, new_resource.user) do
+        gid(new_resource.group)
+        comment('Jenkins slave user - Created by Chef')
+        home(new_resource.remote_fs)
+        system(node['jenkins']['master']['use_system_accounts'])
+      end
     end
 
     #
@@ -177,12 +175,13 @@ class Chef
     # @return [Chef::Resource::Directory]
     #
     def parent_remote_fs_dir_resource
-      return @parent_remote_fs_dir_resource if @parent_remote_fs_dir_resource
-
-      path = ::File.expand_path(new_resource.remote_fs, '..')
-      @parent_remote_fs_dir_resource = Chef::Resource::Directory.new(path, run_context)
-      @parent_remote_fs_dir_resource.recursive(true)
-      @parent_remote_fs_dir_resource
+      @parent_remote_fs_dir_resource ||=
+        begin
+          path = ::File.expand_path(new_resource.remote_fs, '..')
+          build_resource(:directory, path) do
+            recursive(true)
+          end
+        end
     end
 
     #
@@ -193,12 +192,11 @@ class Chef
     # @return [Chef::Resource::Directory]
     #
     def remote_fs_dir_resource
-      return @remote_fs_dir_resource if @remote_fs_dir_resource
-      @remote_fs_dir_resource = Chef::Resource::Directory.new(new_resource.remote_fs, run_context)
-      @remote_fs_dir_resource.owner(new_resource.user)
-      @remote_fs_dir_resource.group(new_resource.group)
-      @remote_fs_dir_resource.recursive(true)
-      @remote_fs_dir_resource
+      @remote_fs_dir_resource ||= build_resource(:directory, new_resource.remote_fs) do
+        owner(new_resource.user)
+        group(new_resource.group)
+        recursive(true)
+      end
     end
 
     #
@@ -209,13 +207,12 @@ class Chef
     # @return [Chef::Resource::RemoteFile]
     #
     def slave_jar_resource
-      return @slave_jar_resource if @slave_jar_resource
-      @slave_jar_resource = Chef::Resource::RemoteFile.new(slave_jar, run_context)
-      @slave_jar_resource.source(slave_jar_url)
-      @slave_jar_resource.backup(false)
-      @slave_jar_resource.mode('0755')
-      @slave_jar_resource.atomic_update(false)
-      @slave_jar_resource
+      @slave_jar_resource ||= build_resource(:remote_file, slave_jar) do
+        source(slave_jar_url)
+        backup(false)
+        mode('0755')
+        atomic_update(false)
+      end
     end
 
     #
@@ -226,23 +223,24 @@ class Chef
     # @return [Chef::Resource::RunitService]
     #
     def service_resource
-      return @service_resource if @service_resource
+      @service_resource ||=
+        begin
+          # Ensure runit is installed on the slave.
+          include_recipe 'runit'
 
-      # Ensure runit is installed on the slave.
-      include_recipe 'runit'
-
-      @service_resource = Chef::Resource::RunitService.new(new_resource.service_name, run_context)
-      @service_resource.cookbook('jenkins')
-      @service_resource.run_template_name('jenkins-slave')
-      @service_resource.log_template_name('jenkins-slave')
-      @service_resource.options(
-        new_resource: new_resource,
-        java_bin:    java,
-        slave_jar:   slave_jar,
-        jnlp_url:    jnlp_url,
-        jnlp_secret: jnlp_secret
-      )
-      @service_resource
+          build_resource(:runit_service, new_resource.service_name) do
+            cookbook('jenkins')
+            run_template_name('jenkins-slave')
+            log_template_name('jenkins-slave')
+            options(
+              new_resource: new_resource,
+              java_bin:    java,
+              slave_jar:   slave_jar,
+              jnlp_url:    jnlp_url,
+              jnlp_secret: jnlp_secret
+            )
+          end
+        end
     end
   end
 end
