dfee477d76906ecf1c1a932bc3b05d1f6320fcdb
Tim Smith <tsmith@chef.io>
2017-04-24 23:33:08 -0700

    Use SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'jenkins'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '5.0.0'
