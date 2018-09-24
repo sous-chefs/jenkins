031c7c4681d0ceaaa7e714631c8a08444d614de4
Josh Barker <josh.barker.developer@gmail.com>
2018-09-24 18:52:50 +1000

    Replace serverspec with InSpec profile for authentication_username_password
    Signed-off-by: Josh Barker <josh.barker.developer@gmail.com>

--- test/integration/authentication_username_password/serverspec/assert_authn_enabled_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe jenkins_plugin('greenballs') do
-  it { should be_a_jenkins_plugin }
-end
-
-describe jenkins_user('random-bob') do
-  it { should be_a_jenkins_user }
-end
