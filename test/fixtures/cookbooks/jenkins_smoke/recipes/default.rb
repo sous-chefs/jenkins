e3d4d59307d5325c1e27baeefd49599c9c107fd8
Josh Barker <josh.barker.developer@gmail.com>
2018-09-24 18:39:49 +1000

    Fix edge case where the disconnected slave is connected
    Signed-off-by: Josh Barker <josh.barker.developer@gmail.com>

--- test/fixtures/cookbooks/jenkins_smoke/recipes/default.rb
+++ test/fixtures/cookbooks/jenkins_smoke/recipes/default.rb
@@ -61,3 +61,34 @@ include_recipe 'jenkins_slave::default'
 # ------------------------------
 include_recipe 'jenkins_user::default'
 
+# The jenkins::_war_package recipe has a delayed restart on the jenkins service
+# When Jenkins starts up, all slaves connect.
+# We need to
+# 1) wait for Jenkins to start
+# 2) ensure the slave is disconnected after the service restart
+ruby_block 'notify last things' do
+  block do
+    # no op
+  end
+  action :run
+
+  notifies :run, 'ruby_block[wait for jenkins to start]', :delayed
+end
+
+ruby_block 'wait for jenkins to start' do
+  block do
+    response = Chef::HTTP.new(node['jenkins']['master']['endpoint']).get('/')
+
+    raise 'Jenkins is starting up' if response.include?('Starting Jenkins')
+  end
+  retries 6
+  retry_delay 5
+  action :nothing
+
+  notifies :disconnect, 'jenkins_slave[disconnect ssh slave at the very end]', :delayed
+end
+
+jenkins_slave 'disconnect ssh slave at the very end' do
+  slave_name 'ssh-to-disconnect'
+  action :nothing
+end
