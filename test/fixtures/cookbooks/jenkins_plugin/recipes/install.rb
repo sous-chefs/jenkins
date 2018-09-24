6b99a9bb49d891f6788ad67e692ef15f8efa51c4
Josh Barker <josh.barker.developer@gmail.com>
2018-09-24 18:28:54 +1000

    In jenkins_plugin, update uninstall, enable and disable recipes so it does not depend on install
    Signed-off-by: Josh Barker <josh.barker.developer@gmail.com>

--- test/fixtures/cookbooks/jenkins_plugin/recipes/install.rb
+++ test/fixtures/cookbooks/jenkins_plugin/recipes/install.rb
@@ -28,9 +28,26 @@ jenkins_plugin 'jquery-ui' do
   install_deps false
 end
 
+# plugin to disable
+jenkins_plugin 'ansicolor' do
+  action :install
+end
+
+# plugin to enable
+jenkins_plugin 'jira-trigger' do
+  action :install
+end
+
+# plugin to uninstall
+jenkins_plugin 'confluence-publisher' do
+  action :install
+end
+
 # Install with a wacky version number
 jenkins_plugin 'build-monitor-plugin' do
   version '1.6+build.135'
   install_deps true
+
+  action :install
   notifies :restart, 'service[jenkins]', :immediately
 end
