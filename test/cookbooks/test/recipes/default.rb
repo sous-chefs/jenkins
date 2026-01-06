ca709c0eb0d519e64423fe76dd6dab8178b136d9
Dan Webb <dan.webb@damacus.io>
2026-01-06 14:39:36 +0000

    fix: use corretto_install for Java 21 LTS support
    The openjdk_pkg_install resource fails on multiple platforms because
    Java 21 packages are not available in OS repositories (Amazon Linux 2023,
    Debian 12, etc.). The sous-chefs/java cookbook hasn't been updated to
    support Java 21 LTS yet.
    
    Solution: Use corretto_install with custom URL to download Amazon Corretto
    21 directly, bypassing OS package repositories. This provides consistent
    Java 21 LTS support across all platforms.

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,9 +1,21 @@
 # Basic Jenkins installation test - no plugins, no auth
 apt_update 'update' if platform_family?('debian')
 
-openjdk_pkg_install 'openjdk' do
-  version '21'
-  action :install
+# Install Amazon Corretto 21 LTS
+# The java cookbook's corretto_install resource supports custom URLs
+# This allows us to use Java 21 LTS which is not yet in the cookbook's defaults
+corretto_21_version = '21.0.5.11.1'
+corretto_install '21' do
+  url lazy {
+    arch = node['kernel']['machine'] == 'aarch64' ? 'aarch64' : 'x64'
+    "https://corretto.aws/downloads/resources/#{corretto_21_version}/amazon-corretto-#{corretto_21_version}-linux-#{arch}.tar.gz"
+  }
+  full_version corretto_21_version
+  java_home lazy {
+    arch = node['kernel']['machine'] == 'aarch64' ? 'aarch64' : 'x64'
+    "/usr/lib/jvm/java-21-corretto/amazon-corretto-#{corretto_21_version}-linux-#{arch}"
+  }
+  bin_cmds %w(jar jarsigner java javac javadoc javap jcmd jconsole jdb jdeprscan jdeps jfr jhsdb jimage jinfo jlink jmap jmod jpackage jps jrunscript jshell jstack jstat jstatd keytool rmid rmiregistry serialver)
 end
 
 node.default['jenkins']['java'] = '/usr/bin/java'
